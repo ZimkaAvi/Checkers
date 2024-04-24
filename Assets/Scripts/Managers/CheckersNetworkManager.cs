7702126271d160780a73f0367e2fad902a45ec02
TERRAGROUP_LABS\mihfi <mih.finikov@gmail.com>
2024-04-24 20:47:03 +0200

    Checkers Network Manager and Lobby Menu Join now work as inteded; Hosting and client connection now works.

--- Assets/Scripts/Managers/CheckersNetworkManager.cs
+++ Assets/Scripts/Managers/CheckersNetworkManager.cs
@@ -10,4 +10,13 @@ public class CheckersNetworkManager : NetworkManager
 {
     [SerializeField] GameObject gameOverHandlerPrefab, boardPrefab, 
         turnsHandlerPrefab;
+
+    public static event Action ClientConnected;
+
+    public override void OnClientConnect()
+    {
+        base.OnClientConnect();
+        ClientConnected!.Invoke();
+        
+    }
 }
--- Assets/Scripts/Menu/JoinLobbyMenu.cs
+++ Assets/Scripts/Menu/JoinLobbyMenu.cs
@@ -9,8 +9,27 @@ public class JoinLobbyMenu : MonoBehaviour
     [SerializeField] GameObject onlinePage;
     [SerializeField] InputField addressInput;
 
-    public void Join()
+    private void OnEnable()
     {
+        CheckersNetworkManager.ClientConnected += OnClientConnected;
+    }
+    private void OnDisable()
+    {
+        CheckersNetworkManager.ClientConnected -= OnClientConnected;
+    }
+
+    private void OnClientConnected()
+    {
+      onlinePage.SetActive(false);
+        gameObject.SetActive(false);
         
     }
+
+
+    public void Join()
+    {
+        NetworkManager.singleton.networkAddress = addressInput.text;
+        NetworkManager.singleton.StartClient();
+    }
+
 }
