002fdf17b718fc324af9000428be031f06e20d7d
TERRAGROUP_LABS\mihfi <mih.finikov@gmail.com>
2024-04-24 20:56:18 +0200

    The player name is now stored.

--- Assets/Scripts/Player/PlayerNetwork.cs
+++ Assets/Scripts/Player/PlayerNetwork.cs
@@ -3,9 +3,18 @@ using Steamworks;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Http.Headers;
 using UnityEngine;
 
 public class PlayerNetwork : Player
 {
-    
+    [SyncVar] string displayName;
+
+    public string DisplayName
+    {
+        get { return displayName; }
+
+        [Server]
+        set { displayName = value; }
+    }
 }
